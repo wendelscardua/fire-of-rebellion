5c324ffc13ef83fe6934032af2158c7a1e16790d
Wendel Scardua <wendelscardua@gmail.com>
2021-12-08 19:32:25 -0300

    tiled wip

---
+++ assets/dungeon/metatiles.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="metatiles" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../metatiles.png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
